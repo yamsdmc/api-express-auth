46bcd24831c0e2e0bed2d6068a9f6c1e2a9f5d77
yamin.gherbi <gherbi.yamin.y@gmail.com>
2024-11-15 20:34:07 +0400

    implement blacklist token & refacto

---
+++ src/domain/services/token-blacklist.ts
@@ -0,0 +1,4 @@
+export interface TokenBlacklistService {
+    addToBlacklist(token: string): Promise<void>;
+    isBlacklisted(token: string): Promise<boolean>;
+}
--- src/infrastructure/http/controllers/auth-controller.ts
+++ src/infrastructure/http/controllers/auth-controller.ts
@@ -2,12 +2,14 @@ import { Request, Response } from 'express';
 import {RegisterUseCase} from "@application/use-cases/auth/register";
 import {LoginUseCase} from "@application/use-cases/auth/login";
 import {RefreshTokenUseCase} from "@application/use-cases/auth/refresh-token";
+import {LogoutUseCase} from "@application/use-cases/auth/logout";
 
 export class AuthController {
     constructor(
         private readonly registerUseCase: RegisterUseCase,
         private readonly loginUseCase: LoginUseCase,
-        private readonly refreshTokenUseCase: RefreshTokenUseCase
+        private readonly logoutUseCase: LogoutUseCase,
+    private readonly refreshTokenUseCase: RefreshTokenUseCase
     ) {}
 
     async register(req: Request, res: Response): Promise<void> {
@@ -37,6 +39,18 @@ export class AuthController {
             res.status(200).json(result);
         } catch (error: any) {
             res.status(401).json({ message: error.message });
+        }
+    }
+
+    async logout(req: Request, res: Response): Promise<void> {
+        try {
+            const token = req.header('Authorization')?.replace('Bearer ', '');
+            const { refreshToken } = req.body;
+
+            await this.logoutUseCase.execute(token!, refreshToken);
+            res.status(200).json({ message: 'Logged out successfully' });
+        } catch (error: any) {
+            res.status(400).json({ message: error.message });
         }
     }
 }
