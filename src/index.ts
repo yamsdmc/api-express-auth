ef23a58d6504e45c6bcfcd2e4bf0dfa5f10bb7b6
yamin.gherbi <gherbi.yamin.y@gmail.com>
2024-12-06 17:48:46 +0100

    add validateEnvironment

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
 import { createApp } from "./app";
+import {validateEnvironment} from "./shared/utils/validateEnv";
 
+validateEnvironment()
 const { app, blacklistService } = createApp("postgresql");
 
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-process.on("SIGTERM", gracefulShutdown);
-process.on("SIGINT", gracefulShutdown);
-
-function gracefulShutdown() {
-  console.log("Shutting down gracefully...");
-  blacklistService.stop();
-  server.close(() => {
-    console.log("Server closed");
-    process.exit(0);
+if(process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+
+  process.on("SIGTERM", gracefulShutdown);
+  process.on("SIGINT", gracefulShutdown);
+
+  function gracefulShutdown() {
+    console.log("Shutting down gracefully...");
+    blacklistService.stop();
+    server.close(() => {
+      console.log("Server closed");
+      process.exit(0);
+    });
+  }
 }
 
-export { app };
+
+export default app;
---
+++ src/shared/utils/validateEnv.ts
@@ -0,0 +1,18 @@
+export const validateEnvironment = () => {
+    const requiredEnvVars = [
+        'NODE_ENV',
+        'JWT_SECRET',
+        'FRONTEND_URL',
+        'RESEND_API_KEY',
+        'DB_HOST',
+        'DB_USER',
+        'DB_PASSWORD',
+        'DB_NAME'
+    ];
+
+    const missingEnvVars = requiredEnvVars.filter(envVar => !process.env[envVar]);
+
+    if (missingEnvVars.length > 0) {
+        throw new Error(`Missing environment variables: ${missingEnvVars.join(', ')}`);
+    }
+}
